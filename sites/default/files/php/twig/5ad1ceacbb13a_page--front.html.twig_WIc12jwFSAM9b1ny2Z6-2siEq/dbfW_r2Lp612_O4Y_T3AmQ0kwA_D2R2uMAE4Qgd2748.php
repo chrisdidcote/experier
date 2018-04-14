e85a0288c43fd1c9375293f5fea4df1e57eb0176
chrisdidcote <chris.didcote@computerminds.co.uk>
2018-04-14 13:49:18 +0100

    rough build

---
+++ sites/default/files/php/twig/5ad1ceacbb13a_page--front.html.twig_WIc12jwFSAM9b1ny2Z6-2siEq/dbfW_r2Lp612_O4Y_T3AmQ0kwA_D2R2uMAE4Qgd2748.php
@@ -0,0 +1,355 @@
+<?php
+
+/* themes/custom/experier/templates/system/page--front.html.twig */
+class __TwigTemplate_5d2df2091ac783fbb28066ceee5ef5b191f4eaaada24fd2c12123220ea6db1b0 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+            'navbar' => array($this, 'block_navbar'),
+            'main' => array($this, 'block_main'),
+            'header' => array($this, 'block_header'),
+            'highlighted' => array($this, 'block_highlighted'),
+            'help' => array($this, 'block_help'),
+            'content' => array($this, 'block_content'),
+            'sidebar_first' => array($this, 'block_sidebar_first'),
+            'sidebar_second' => array($this, 'block_sidebar_second'),
+            'footer' => array($this, 'block_footer'),
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("set" => 54, "if" => 56, "block" => 57);
+        $filters = array("clean_class" => 62, "t" => 74);
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('set', 'if', 'block'),
+                array('clean_class', 't'),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 54
+        $context["container"] = (($this->getAttribute($this->getAttribute(($context["theme"] ?? null), "settings", array()), "fluid_container", array())) ? ("container-fluid") : ("container"));
+        // line 56
+        if (($this->getAttribute(($context["page"] ?? null), "navigation", array()) || $this->getAttribute(($context["page"] ?? null), "navigation_collapsible", array()))) {
+            // line 57
+            echo "  ";
+            $this->displayBlock('navbar', $context, $blocks);
+        }
+        // line 94
+        echo "
+";
+        // line 96
+        $this->displayBlock('main', $context, $blocks);
+        // line 162
+        echo "
+";
+        // line 163
+        if ($this->getAttribute(($context["page"] ?? null), "footer", array())) {
+            // line 164
+            echo "  ";
+            $this->displayBlock('footer', $context, $blocks);
+        }
+    }
+
+    // line 57
+    public function block_navbar($context, array $blocks = array())
+    {
+        // line 58
+        echo "    ";
+        // line 59
+        $context["navbar_classes"] = array(0 => "navbar", 1 => (($this->getAttribute($this->getAttribute(        // line 61
+($context["theme"] ?? null), "settings", array()), "navbar_inverse", array())) ? ("navbar-inverse") : ("navbar-default")), 2 => (($this->getAttribute($this->getAttribute(        // line 62
+($context["theme"] ?? null), "settings", array()), "navbar_position", array())) ? (("navbar-" . \Drupal\Component\Utility\Html::getClass($this->getAttribute($this->getAttribute(($context["theme"] ?? null), "settings", array()), "navbar_position", array())))) : (($context["container"] ?? null))));
+        // line 65
+        echo "    <header";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["navbar_attributes"] ?? null), "addClass", array(0 => ($context["navbar_classes"] ?? null)), "method"), "html", null, true));
+        echo " id=\"navbar\" role=\"banner\">
+      ";
+        // line 66
+        if ( !$this->getAttribute(($context["navbar_attributes"] ?? null), "hasClass", array(0 => ($context["container"] ?? null)), "method")) {
+            // line 67
+            echo "        <div class=\"";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["container"] ?? null), "html", null, true));
+            echo "\">
+      ";
+        }
+        // line 69
+        echo "      <div class=\"navbar-header\">
+        ";
+        // line 70
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "navigation", array()), "html", null, true));
+        echo "
+        ";
+        // line 72
+        echo "        ";
+        if ($this->getAttribute(($context["page"] ?? null), "navigation_collapsible", array())) {
+            // line 73
+            echo "          <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#navbar-collapse\">
+            <span class=\"sr-only\">";
+            // line 74
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("Toggle navigation")));
+            echo "</span>
+            <span class=\"icon-bar\"></span>
+            <span class=\"icon-bar\"></span>
+            <span class=\"icon-bar\"></span>
+          </button>
+        ";
+        }
+        // line 80
+        echo "      </div>
+
+      ";
+        // line 83
+        echo "      ";
+        if ($this->getAttribute(($context["page"] ?? null), "navigation_collapsible", array())) {
+            // line 84
+            echo "        <div id=\"navbar-collapse\" class=\"navbar-collapse collapse\">
+          ";
+            // line 85
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "navigation_collapsible", array()), "html", null, true));
+            echo "
+        </div>
+      ";
+        }
+        // line 88
+        echo "      ";
+        if ( !$this->getAttribute(($context["navbar_attributes"] ?? null), "hasClass", array(0 => ($context["container"] ?? null)), "method")) {
+            // line 89
+            echo "        </div>
+      ";
+        }
+        // line 91
+        echo "    </header>
+  ";
+    }
+
+    // line 96
+    public function block_main($context, array $blocks = array())
+    {
+        // line 97
+        echo "  <div role=\"main\" class=\"main-container ";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["container"] ?? null), "html", null, true));
+        echo " js-quickedit-main-content\">
+    <div class=\"row\">
+
+      ";
+        // line 101
+        echo "      ";
+        if ($this->getAttribute(($context["page"] ?? null), "header", array())) {
+            // line 102
+            echo "        ";
+            $this->displayBlock('header', $context, $blocks);
+            // line 107
+            echo "      ";
+        }
+        // line 108
+        echo "
+
+      ";
+        // line 111
+        echo "      ";
+        // line 112
+        $context["content_classes"] = array(0 => ((($this->getAttribute(        // line 113
+($context["page"] ?? null), "sidebar_first", array()) && $this->getAttribute(($context["page"] ?? null), "sidebar_second", array()))) ? ("col-sm-6") : ("")), 1 => ((($this->getAttribute(        // line 114
+($context["page"] ?? null), "sidebar_first", array()) && twig_test_empty($this->getAttribute(($context["page"] ?? null), "sidebar_second", array())))) ? ("col-sm-9") : ("")), 2 => ((($this->getAttribute(        // line 115
+($context["page"] ?? null), "sidebar_second", array()) && twig_test_empty($this->getAttribute(($context["page"] ?? null), "sidebar_first", array())))) ? ("col-sm-9") : ("")), 3 => (((twig_test_empty($this->getAttribute(        // line 116
+($context["page"] ?? null), "sidebar_first", array())) && twig_test_empty($this->getAttribute(($context["page"] ?? null), "sidebar_second", array())))) ? ("col-sm-12") : ("")));
+        // line 119
+        echo "      <section";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["content_attributes"] ?? null), "addClass", array(0 => ($context["content_classes"] ?? null)), "method"), "html", null, true));
+        echo ">
+
+        ";
+        // line 122
+        echo "        ";
+        if ($this->getAttribute(($context["page"] ?? null), "highlighted", array())) {
+            // line 123
+            echo "          ";
+            $this->displayBlock('highlighted', $context, $blocks);
+            // line 126
+            echo "        ";
+        }
+        // line 127
+        echo "
+        ";
+        // line 129
+        echo "        ";
+        if ($this->getAttribute(($context["page"] ?? null), "help", array())) {
+            // line 130
+            echo "          ";
+            $this->displayBlock('help', $context, $blocks);
+            // line 133
+            echo "        ";
+        }
+        // line 134
+        echo "
+        ";
+        // line 136
+        echo "        ";
+        $this->displayBlock('content', $context, $blocks);
+        // line 140
+        echo "      </section>
+      
+      ";
+        // line 143
+        echo "      ";
+        if ($this->getAttribute(($context["page"] ?? null), "sidebar_first", array())) {
+            // line 144
+            echo "        ";
+            $this->displayBlock('sidebar_first', $context, $blocks);
+            // line 149
+            echo "      ";
+        }
+        // line 150
+        echo "
+      ";
+        // line 152
+        echo "      ";
+        if ($this->getAttribute(($context["page"] ?? null), "sidebar_second", array())) {
+            // line 153
+            echo "        ";
+            $this->displayBlock('sidebar_second', $context, $blocks);
+            // line 158
+            echo "      ";
+        }
+        // line 159
+        echo "    </div>
+  </div>
+";
+    }
+
+    // line 102
+    public function block_header($context, array $blocks = array())
+    {
+        // line 103
+        echo "          <div class=\"col-sm-12\" role=\"heading\">
+            ";
+        // line 104
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "header", array()), "html", null, true));
+        echo "
+          </div>
+        ";
+    }
+
+    // line 123
+    public function block_highlighted($context, array $blocks = array())
+    {
+        // line 124
+        echo "            <div class=\"highlighted\">";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "highlighted", array()), "html", null, true));
+        echo "</div>
+          ";
+    }
+
+    // line 130
+    public function block_help($context, array $blocks = array())
+    {
+        // line 131
+        echo "            ";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "help", array()), "html", null, true));
+        echo "
+          ";
+    }
+
+    // line 136
+    public function block_content($context, array $blocks = array())
+    {
+        // line 137
+        echo "          <a id=\"main-content\"></a>
+          ";
+        // line 138
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "content", array()), "html", null, true));
+        echo "
+        ";
+    }
+
+    // line 144
+    public function block_sidebar_first($context, array $blocks = array())
+    {
+        // line 145
+        echo "          <aside class=\"col-sm-3\" role=\"complementary\">
+            ";
+        // line 146
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "sidebar_first", array()), "html", null, true));
+        echo "
+          </aside>
+        ";
+    }
+
+    // line 153
+    public function block_sidebar_second($context, array $blocks = array())
+    {
+        // line 154
+        echo "          <aside class=\"col-sm-3\" role=\"complementary\">
+            ";
+        // line 155
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "sidebar_second", array()), "html", null, true));
+        echo "
+          </aside>
+        ";
+    }
+
+    // line 164
+    public function block_footer($context, array $blocks = array())
+    {
+        // line 165
+        echo "    <footer class=\"footer ";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["container"] ?? null), "html", null, true));
+        echo "\" role=\"contentinfo\">
+      ";
+        // line 166
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "footer", array()), "html", null, true));
+        echo "
+    </footer>
+  ";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/experier/templates/system/page--front.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  322 => 166,  317 => 165,  314 => 164,  307 => 155,  304 => 154,  301 => 153,  294 => 146,  291 => 145,  288 => 144,  282 => 138,  279 => 137,  276 => 136,  269 => 131,  266 => 130,  259 => 124,  256 => 123,  249 => 104,  246 => 103,  243 => 102,  237 => 159,  234 => 158,  231 => 153,  228 => 152,  225 => 150,  222 => 149,  219 => 144,  216 => 143,  212 => 140,  209 => 136,  206 => 134,  203 => 133,  200 => 130,  197 => 129,  194 => 127,  191 => 126,  188 => 123,  185 => 122,  179 => 119,  177 => 116,  176 => 115,  175 => 114,  174 => 113,  173 => 112,  171 => 111,  167 => 108,  164 => 107,  161 => 102,  158 => 101,  151 => 97,  148 => 96,  143 => 91,  139 => 89,  136 => 88,  130 => 85,  127 => 84,  124 => 83,  120 => 80,  111 => 74,  108 => 73,  105 => 72,  101 => 70,  98 => 69,  92 => 67,  90 => 66,  85 => 65,  83 => 62,  82 => 61,  81 => 59,  79 => 58,  76 => 57,  70 => 164,  68 => 163,  65 => 162,  63 => 96,  60 => 94,  56 => 57,  54 => 56,  52 => 54,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "themes/custom/experier/templates/system/page--front.html.twig", "/Applications/MAMP/htdocs/experier/themes/custom/experier/templates/system/page--front.html.twig");
+    }
+}
